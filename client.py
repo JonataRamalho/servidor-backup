d9e108340f19b810d02bf0af2ca7e23851972749
LuhBezerra <lucileneifal2018@gmail.com>
2020-11-02 18:23:32 -0300

    feat: adiciona esqueleto do cliente

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 60000  
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(str.encode('Testando envio de mensagens'))
+data = s.recv(1024)
+
+print('>>> Resposta do servidor:', data.decode())
