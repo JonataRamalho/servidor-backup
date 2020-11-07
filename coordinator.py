09325a625789648628edfe7b144199ad4dcddc63
JonataRamalho <jonataramalho.s@gmail.com>
2020-11-07 20:51:17 -0300

    feat: seleciona o método escolhido

--- coordinator.py
+++ coordinator.py
@@ -7,8 +7,29 @@ address = (hostClient, portClient)
 
 def connectDataChannel(connection, clientIP):
     option = connection.recv(1024)
+    select(option.decode())
 
-    message = 'ok'
+def select(option):
+    if option == '1':
+        transferFile()
+    elif option == '2':
+        listFile()
+    else:
+        downloadFile()
+    
+def transferFile():
+    message = 'Opção selecionada >> Transmitir arquivo'
+    send(message)
+
+def listFile():
+    message = 'Opção selecionada >> Listar arquivos'
+    send(message)
+
+def downloadFile():
+    message = 'Opção selecionada >> Baixar arquivo'
+    send(message)
+
+def send(message):
     connection.sendall(str.encode(message))
     connection.close()
 
