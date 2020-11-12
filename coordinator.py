d696ff005cc23a209ebba9d4fac4c55af6ebf970
JonataRamalho <jonataramalho.s@gmail.com>
2020-11-12 10:06:32 -0300

    fix: atualiza função connectDataChannel

--- coordinator.py
+++ coordinator.py
@@ -2,10 +2,47 @@ import socket
 import threading
 
 def connectDataChannel():
-    print('Thread 1 ok')
+    client = createSocketClient()
+
+    acceptConnection(client)
 
 def connectControlChannel(): 
-    print('Thread 2 ok')
+    print('Thread canal de controle ')
+
+def createSocketClient():
+    address = informClientAddress()
+
+    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socketClient.bind(address)
+    socketClient.listen()
+
+    return socketClient
+
+def informClientAddress():
+    hostClient = ''
+    portClient = 10000
+
+    return (hostClient, portClient)
+
+def acceptConnection(client):
+    while True:
+        connection, clientIP = client.accept()
+
+        createClientThread(connection, clientIP)
+
+def createClientThread(connection, clientIP):
+    print('Conectado por', clientIP)
+
+    clientThread = threading.Thread(target=teste, args=(connection,))
+    clientThread.start()
+
+def teste(connection):
+    data = connection.recv(1024)
+
+    connection.sendall(data)
+    
+    print('Coordenador se comunicou com Cliente \n')
 
 def select(option):
     if option == '1':
